ec78b8d517a5797dfc8ee8f62fc2f7a216f322fc
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-12 02:45:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+
+function deepestChild() {
+  var lis = document.getElementById('grand-node')
+  var next = lis.children[0]
+  while (next) {
+    lis = next
+    next = lis.children[0]
+  }
+  return lis
+  }
