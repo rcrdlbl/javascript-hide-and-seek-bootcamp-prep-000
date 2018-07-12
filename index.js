6a343464d1fa676651a8f5bb26920861ed151196
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-12 02:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function deepestChild() {
   }
   return lis
   }
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+  }
+}
